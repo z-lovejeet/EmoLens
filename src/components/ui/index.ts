271a10b6529d7119d7627a5a4c5ad2b3d129af31
z-lovejeet <lovejeet1225@gmail.com>
2026-08-04 19:59:30 +0530

    feat: add UI component barrel export index

---
+++ src/components/ui/index.ts
@@ -0,0 +1,9 @@
+// UI Component exports
+// All components are co-located with their CSS modules
+
+export { Button } from './Button';
+export { Card } from './Card';
+export { Chip } from './Chip';
+export { Slider } from './Slider';
+export { Toast } from './Toast';
+export { Skeleton, SkeletonCard } from './Skeleton';
